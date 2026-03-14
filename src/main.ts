2ec8a4098fbc32e6e70b49fdf98ea66cb6a831cd
Daniel Gione <daniel.gione@gmail.com>
2026-03-14 10:57:23 -0700

    todos, added sharp

--- src/main.ts
+++ src/main.ts
@@ -19,18 +19,17 @@ const service = new INaturalistService();
 const scientificName: string = 'Plagiobothrys Acanthocarpus';
 
 // TODO get all scientific names of the native plants to use for pulling observations to cycle thru
+// TODO limit api hits to 1 per second
 
+// TAXA is good for the best photo, maybe do a secondary set of photos from observations for each?
 service.getTaxa(scientificName).pipe(
     switchMap((x: Response) => defer(() => x.json())),
     tap((json: any) => {
         json.results.forEach((observation: any) => {
             if (observation.default_photo != null){
                 observation.default_photo.url =  observation.default_photo.url.replace('square', 'original');
-                console.log('defaultPhoto:', observation.default_photo);
             }
         });
     }),
 ).subscribe();
 
-
-// TODO limit api hits to 1 per second
