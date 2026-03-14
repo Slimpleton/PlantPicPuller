596125076100d10adbd846bca9d45e7fed357988
Daniel Gione <daniel.gione@gmail.com>
2026-03-14 10:49:13 -0700

    Getting taxonId for most accurate results, using taxon default photo to get the most detailed / accurate photos for the species

--- src/inaturalist.service.ts
+++ src/inaturalist.service.ts
@@ -1,5 +1,5 @@
-import {fromFetch} from 'rxjs/fetch';
-import { Observable } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { map, Observable, switchMap } from 'rxjs';
 
 export interface Observation {
 
@@ -10,18 +10,55 @@ export class INaturalistService {
     public constructor() { }
 
     public getObservation(scientificName: string): Observable<Response> {
-        const perPageAmount: number = 200;
-        const params = new URLSearchParams ({
-            'iconic_taxa': 'Plantae',
-            'quality_grade': 'research',
-            'license': 'cc0,cc-by',
-            'photo_license': 'cc0,cc-by',
-            'taxon_name': scientificName,
-            'per_page': String(perPageAmount),
-            'order_by':'created_at'
-        });
+        const perPageAmount: number = 25;
 
-        const url: URL = new URL(`${INaturalistService._BASE_URL}observations?${params}`);
-        return fromFetch(url.toString());
+        return this.getTaxon(scientificName).pipe(
+            switchMap((id) => {
+                const params = new URLSearchParams({
+                    'quality_grade': 'research',
+                    'license': 'cc0,cc-by',
+                    'photo_license': 'cc0,cc-by',
+                    'taxon_id': String(id),
+                    'per_page': String(perPageAmount),
+                    'order_by': 'votes',
+                    'order': 'desc',
+                });
+        
+                const url: URL = new URL(`${INaturalistService._BASE_URL}observations?${params}`);
+                return fromFetch(url.toString());
+            }));
+    }
+
+    public getTaxa(scientificName: string): Observable<Response> {
+        const perPageAmount: number = 25;
+
+        return this.getTaxon(scientificName).pipe(
+            switchMap((id: number) =>{
+                const params = new URLSearchParams({
+                    'quality_grade': 'research',
+                    'license': 'cc0,cc-by',
+                    'photo_license': 'cc0,cc-by',
+                    'taxon_id': String(id),
+                    'per_page': String(perPageAmount),
+                    'order_by': 'votes',
+                    'order': 'desc',
+                });
+        
+                const url: URL = new URL(`${INaturalistService._BASE_URL}taxa?${params}`);
+                return fromFetch(url.toString());
+            })
+        );
+    }
+
+    private getTaxon(scientificName: string): Observable<number> {
+        const taxaParams = new URLSearchParams({
+            'q': scientificName,
+            'rank': 'species',
+            'is_active': 'true',
+            'per_page': '1'
+        });
+        return fromFetch(`${INaturalistService._BASE_URL}taxa?${taxaParams}`).pipe(
+            switchMap((res) => res.json()),
+            map((json) => json.results[0].id));
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -20,17 +20,17 @@ const scientificName: string = 'Plagiobothrys Acanthocarpus';
 
 // TODO get all scientific names of the native plants to use for pulling observations to cycle thru
 
-service.getObservation(scientificName).pipe(
+service.getTaxa(scientificName).pipe(
     switchMap((x: Response) => defer(() => x.json())),
     tap((json: any) => {
         json.results.forEach((observation: any) => {
-            if (observation.photos != null)
-                observation.photos.forEach((photo: Photo) => {
-                   photo.url = photo.url.replace('square', 'original');
-                });
-            console.log(observation.photos);
+            if (observation.default_photo != null){
+                observation.default_photo.url =  observation.default_photo.url.replace('square', 'original');
+                console.log('defaultPhoto:', observation.default_photo);
+            }
         });
     }),
 ).subscribe();
+
 
 // TODO limit api hits to 1 per second
